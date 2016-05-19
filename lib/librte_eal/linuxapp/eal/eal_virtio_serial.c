08a6087e838f86f12bac3395f8cf1b0e7b93aa42
Mauricio Vasquez B <mauricio.vasquezbernal@studenti.polito.it>
2016-05-19 10:42:34 +0200

    virtio-serial: rename functions to normal and bypass

--- lib/librte_eal/linuxapp/eal/eal_virtio_serial.c
+++ lib/librte_eal/linuxapp/eal/eal_virtio_serial.c
@@ -82,7 +82,7 @@ process_host_request(char * buf, size_t len)
 		err = sscanf(strtok(NULL, ","), "new=%s", p_new);
 		if (err != 1)
 			goto error;
-		err = rte_eth_add_slave_to_ring(p_old, p_new);
+		err = rte_eth_add_bypass_to_ring(p_old, p_new);
 		if (err != 0)
 			goto error;
 	} else if (!strcmp(action, "del")) {
@@ -90,7 +90,7 @@ process_host_request(char * buf, size_t len)
 		err = sscanf(str, "old=%s", p_old);
 		if (err != 1)
 			goto error;
-		err = rte_eth_remove_slave_from_ring(p_old);
+		err = rte_eth_remove_bypass_from_ring(p_old);
 		if (err != 0)
 			goto error;
 	} else {
